1a233f167bc88effdf27412b7214947d64f764c9
Santhosh Kumar <skumar.hpa@gmail.com>
2024-10-09 12:34:06 +0100

    updated properties

---
+++ src/EPR.Calculator.Service.Function/ServiceBusQueueTrigger.cs
@@ -0,0 +1,15 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace EPR.Calculator.Service.Function
+{
+    public static class ServiceBusQueueTrigger
+    {
+        [FunctionName("ServiceBusQueueTrigger")]
+        public static void Run([ServiceBusTrigger("defra.epr.calculator.run", Connection = "AzureWebJobServiceBus")] string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# Service Bus Queue trigger function proceed messages: {myQueueItem}");
+
+        }
+    }
+}
