ee1ad825a29186baf69db0f244edb89ee8bdbb97
Santhosh Kumar <skumar.hpa@gmail.com>
2024-10-10 16:49:21 +0100

    inlcude unit tets project

---
+++ src/EPR.Calculator.Service.Function.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EPR.Calculator.Service.AzureFuntions.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
