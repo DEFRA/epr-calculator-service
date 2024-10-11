82f9694e568aeea38c5e9e25f1eb168d2e36c8f4
Santhosh Kumar <skumar.hpa@gmail.com>
2024-10-11 17:40:07 +0100

    updated the queue name

--- src/EPR.Calculator.Service.Function/ServiceBusQueueTrigger.cs
+++ src/EPR.Calculator.Service.Function/ServiceBusQueueTrigger.cs
@@ -6,7 +6,7 @@ namespace EPR.Calculator.Service.Function
     public static class ServiceBusQueueTrigger
     {
         [FunctionName("ServiceBusQueueTrigger")]
-        public static void Run([ServiceBusTrigger(queueName: "ServiceBusQueueName", Connection = "ServiceBusConnectionString")] string myQueueItem, ILogger log)
+        public static void Run([ServiceBusTrigger(queueName: "defra.epr.calculator.run.dev11", Connection = "ServiceBusConnectionString")] string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Service Bus Queue trigger function proceed messages: {myQueueItem}");
 
