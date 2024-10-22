64eb6c780bf225f6527a54e34dc21429492651d7
Santhosh Kumar <skumar.hpa@gmail.com>
2024-10-22 17:02:22 +0100

    add missing files

--- src/EPR.Calculator.Service.Function/Mapper/CalculatorRunParameterMapper.cs
+++ src/EPR.Calculator.Service.Function/Mapper/CalculatorRunParameterMapper.cs
@@ -1,12 +1,10 @@
 ﻿using EPR.Calculator.Service.Common;
 
 
-
 namespace EPR.Calculator.Service.Function.Mapper
 {
     public class CalculatorRunParameterMapper 
     {
-
         public static CalculatorRunParameter Map(CalculatorParameter calculatorParameter)
         {
             return new CalculatorRunParameter()
