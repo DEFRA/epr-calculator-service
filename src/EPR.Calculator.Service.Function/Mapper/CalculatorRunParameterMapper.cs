034a73aab0115c5d27d12686aab83dcffcd38538
Santhosh Kumar <skumar.hpa@gmail.com>
2024-10-22 15:59:08 +0100

    Implemented orchestrator

---
+++ src/EPR.Calculator.Service.Function/Mapper/CalculatorRunParameterMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.Service.Function.Mapper
+{
+    internal class CalculatorRunParameterMapper
+    {
+    }
+}
---
+++ src/EPR.Calculator.Service.Function/Startup.cs
@@ -0,0 +1,27 @@
+﻿using EPR.Calculator.Service.Function.Interface;
+using EPR.Calculator.Service.Function.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.Service.Function
+{
+    public class Startup : FunctionsStartup
+    {
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            RegisterDependencies(builder.Services);
+        }
+
+        private void RegisterDependencies(IServiceCollection services)
+        {
+            services.AddTransient<ICalculatorRunService, CalculatorRunService>();
+        }
+
+    }
+}
