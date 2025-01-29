6df2f69a3c639e015b75b80cfc5078b3a88ee7c5
Tim Powell <timothy.powell@eviden.com>
2025-01-29 11:59:04 +0000

    Add transpose service files

---
+++ src/EPR.Calculator.Service.Function/Interface/ITransposePomAndOrgDataService.cs
@@ -0,0 +1,11 @@
+﻿namespace EPR.Calculator.Service.Function.Interface
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using EPR.Calculator.API.Dtos;
+
+    public interface ITransposePomAndOrgDataService
+    {
+        Task<bool> Transpose(CalcResultsRequestDto resultsRequestDto, CancellationToken cancellationToken);
+    }
+}
