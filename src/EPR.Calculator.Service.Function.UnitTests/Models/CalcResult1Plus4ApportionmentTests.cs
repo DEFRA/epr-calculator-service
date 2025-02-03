70d820e0ce84d4111249d0916bbe278d1d91ee63
Tim Powell <timothy.powell@eviden.com>
2025-02-03 16:36:54 +0000

    Add more unit tests

---
+++ src/EPR.Calculator.Service.Function.UnitTests/Models/CalcResult1Plus4ApportionmentTests.cs
@@ -0,0 +1,36 @@
+namespace EPR.Calculator.Service.Function.UnitTests.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using AutoFixture;
+    using EPR.Calculator.Service.Function.Models;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class CalcResult1Plus4ApportionmentTests
+    {
+        private CalcResult1Plus4Apportionment TestClass;
+        private IFixture Fixture;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Fixture = new Fixture();
+            this.TestClass = new CalcResult1Plus4Apportionment();
+        }
+
+        [TestMethod]
+        public void CanSetAndGetCalcResultParameterCommunicationCostDetails()
+        {
+            // Arrange
+            var testValue = new Mock<IEnumerable<CalcResultParameterCostDetail>>().Object;
+
+            // Act
+            this.TestClass.CalcResultParameterCommunicationCostDetails = testValue;
+
+            // Assert
+            Assert.AreSame(testValue, this.TestClass.CalcResultParameterCommunicationCostDetails);
+        }
+    }
+}
