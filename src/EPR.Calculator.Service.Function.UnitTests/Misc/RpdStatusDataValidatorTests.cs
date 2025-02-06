d61171c662342aa3f9362ee46aa46df02417b29c
Tim Powell <timothy.powell@eviden.com>
2025-02-06 10:20:42 +0000

    Add more unit tests

--- src/EPR.Calculator.Service.Function.UnitTests/Misc/RpdStatusDataValidatorTests.cs
+++ src/EPR.Calculator.Service.Function.UnitTests/Misc/RpdStatusDataValidatorTests.cs
@@ -6,6 +6,7 @@ namespace EPR.Calculator.Service.Function.UnitTests
     using EPR.Calculator.Service.Function.Data.DataModels;
     using EPR.Calculator.Service.Function.Enums;
     using EPR.Calculator.Service.Function.Interface;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
 
@@ -18,6 +19,8 @@ namespace EPR.Calculator.Service.Function.UnitTests
 
         private Mock<IOrgAndPomWrapper> Wrapper { get; set; }
 
+        private IEnumerable<CalculatorRunClassification> CalculatorRunClassifications { get; set; }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="RpdStatusDataValidatorTests"/> class.
         /// </summary>
@@ -26,6 +29,20 @@ namespace EPR.Calculator.Service.Function.UnitTests
             this.Fixture = new Fixture();
             this.Wrapper = new Mock<IOrgAndPomWrapper>();
             this.TestClass = new RpdStatusDataValidator(this.Wrapper.Object);
+
+            this.CalculatorRunClassifications = new List<CalculatorRunClassification>
+            {
+                new CalculatorRunClassification
+                {
+                    Id = (int)RunClassification.INTHEQUEUE,
+                    Status = "IN THE QUEUE",
+                },
+                new CalculatorRunClassification
+                {
+                    Id = (int) RunClassification.RUNNING,
+                    Status = RunClassification.RUNNING.ToString(),
+                },
+            };
         }
 
         [TestMethod]
@@ -39,7 +56,49 @@ namespace EPR.Calculator.Service.Function.UnitTests
         }
 
         [TestMethod]
-        public async Task UpdateRpdStatus_With_RunId_Having_OrganisationDataMasterId()
+        [DataRow(RunClassification.INTHEQUEUE)]
+        [DataRow(RunClassification.RUNNING)]
+        public async Task IsValidRun_Success(RunClassification runClassification)
+        {
+            // Arrange
+            var runId = this.Fixture.Create<int>();
+            var run = this.Fixture.Create<CalculatorRun>();
+            run.Id = runId;
+            run.CalculatorRunOrganisationDataMasterId = null;
+            run.CalculatorRunPomDataMasterId = null;
+            run.CalculatorRunClassificationId = (int)runClassification;
+
+            // Act
+            var result = this.TestClass.IsValidRun(
+                run,
+                runId,
+                this.CalculatorRunClassifications);
+
+            // Assert
+            Assert.IsTrue(result.isValid);
+            Assert.IsNull(result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public async Task IsValidRun_With_CalcRun_Missing()
+        {
+            // Arrange
+            var runId = this.Fixture.Create<int>();
+
+            // Act
+            var result = this.TestClass.IsValidRun(
+                null,
+                runId,
+                this.Fixture.CreateMany<CalculatorRunClassification>());
+
+            // Assert
+            Assert.IsFalse(result.isValid);
+            Assert.AreEqual(StatusCodes.Status400BadRequest, result.StatusCode);
+            Assert.AreEqual($"Calculator Run {runId} is missing", result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public async Task IsValidRun_With_RunId_Having_OrganisationDataMasterId()
         {
             // Arrange
             var runId = this.Fixture.Create<int>();
@@ -54,12 +113,12 @@ namespace EPR.Calculator.Service.Function.UnitTests
 
             // Assert
             Assert.IsFalse(result.isValid);
-            Assert.AreEqual(422, result.StatusCode);
+            Assert.AreEqual(StatusCodes.Status422UnprocessableEntity, result.StatusCode);
             Assert.AreEqual($"Calculator Run {runId} already has OrganisationDataMasterId associated with it", result.ErrorMessage);
         }
 
         [TestMethod]
-        public async Task UpdateRpdStatus_With_RunId_Having_PomDataMasterId()
+        public async Task IsValidRun_With_RunId_Having_PomDataMasterId()
         {
             // Arrange
             var runId = this.Fixture.Create<int>();
@@ -75,12 +134,12 @@ namespace EPR.Calculator.Service.Function.UnitTests
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.AreEqual(422, result.StatusCode);
+            Assert.AreEqual(StatusCodes.Status422UnprocessableEntity, result.StatusCode);
             Assert.AreEqual($"Calculator Run {runId} already has PomDataMasterId associated with it", result.ErrorMessage);
         }
 
         [TestMethod]
-        public async Task UpdateRpdStatus_With_RunId_With_Incorrect_Classification()
+        public async Task IsValidRun_With_RunId_With_Incorrect_Classification()
         {
             // Arrange
             var runId = this.Fixture.Create<int>();
@@ -100,15 +159,35 @@ namespace EPR.Calculator.Service.Function.UnitTests
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.AreEqual(422, result.StatusCode);
+            Assert.AreEqual(StatusCodes.Status422UnprocessableEntity, result.StatusCode);
             Assert.AreEqual($"Calculator Run {runId} classification should be RUNNING or IN THE QUEUE", result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public async Task IsValidSuccessfullRun_Success()
+        {
+            // Arrange
+            var runId = this.Fixture.Create<int>();
+            var run = this.Fixture.Create<CalculatorRun>();
+            run.Id = runId;
+            run.CalculatorRunOrganisationDataMasterId = null;
+            run.CalculatorRunPomDataMasterId = null;
+
+            this.Wrapper.Setup(w => w.AnyPomData()).Returns(true);
+            this.Wrapper.Setup(w => w.AnyOrganisationData()).Returns(true);
+
+            // Act
+            var result = this.TestClass.IsValidSuccessfulRun(runId);
+
+            Assert.IsTrue(result.isValid);
+            Assert.IsNull(result.ErrorMessage);
         }
 
         [TestMethod]
         [DataRow(false, true)]
         [DataRow(true, false)]
         [DataRow(false, false)]
-        public async Task UpdateRpdStatus_With_RunId_Having_Pom_Data_Missing(bool pomDataExists, bool orgDataExists)
+        public async Task IsValidSuccessfullRun_With_RunId_Having_Pom_Data_Missing(bool pomDataExists, bool orgDataExists)
         {
             // Arrange
             var runId = this.Fixture.Create<int>();
@@ -123,7 +202,7 @@ namespace EPR.Calculator.Service.Function.UnitTests
             // Act
             var result = this.TestClass.IsValidSuccessfulRun(runId);
 
-            Assert.AreEqual(422, result.StatusCode);
+            Assert.AreEqual(StatusCodes.Status422UnprocessableEntity, result.StatusCode);
             Assert.AreEqual("PomData or Organisation Data is missing", result.ErrorMessage);
         }
     }
