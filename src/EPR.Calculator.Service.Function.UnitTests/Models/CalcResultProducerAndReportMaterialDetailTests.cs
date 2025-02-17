c8719c2bcd02920f00678694e7015a58a5c7ae67
Uday Denduluri <uday.2.denduluri.external@eviden.com>
2025-02-17 15:07:19 +0000

    Feature/498610 extrapolation ref data columns (#34)
    * 498610 Extrapolation ref data columns
    
    * Adding Unit tests - UD
    
    * Update CalcResultsExporter.cs
    
    * remove ProduderDetails and using run tables
    
    * Refactor of Submission Period  - UD
    
    * Clean up
    
    * Fix Query
    
    * Update CalcResultScaledupProducersBuilder.cs
    
    * 498610 Extrapolation ref data columns
    
    * 498610 Extrapolation ref data columns
    
    * Fixed the Level index
    
    * Fixed totals for one column
    
    * 498610 Extrapolation ref data columns
    
    * Order by Fixes
    
    * Fix unit test
    
    * Create and fix unit tests
    
    * Update EPR.Calculator.Service.Function.csproj
    
    * refactored code
    
    * Refactoring TotalRow
    
    * Update CalcResultsExporterTests.cs
    
    * Add ExtraRows Unit test
    
    * Deleting ScaledUpUtil
    
    * Add unit tests to exporter
    
    * Update CalcResultsExporterTests.cs
    
    * GetOverallTotalRowTest
    
    * Update CalcResultsExporterTests.cs
    
    * GetProducerReportedMaterialsAsyncTest
    
    * GetTonnagesTest
    
    * Unit test for ColumnHeaders and MaterialBreakdown
    
    * More unit tests
    
    * Fix the test
    
    * ScaleupProducerTest
    
    * Create ScaleupProducerTests.cs
    
    * 498610 Extrapolation ref data columns
    
    * Update CalcResultScaledupProducersBuilderTest.cs
    
    * Update CalcResultScaledupProducersBuilderTest.cs
    
    * Fix some sonarqube warnings
    
    * Adding missing test
    
    * Fix failing test
    
    * Update CalcResultScaledupProducersBuilder.cs
    
    * Fix for Net Reported Tonnage value
    
    ---------
    
    Co-authored-by: johnbenjamin-defra <john.2.benjamin.external@eviden.com>
    Co-authored-by: Tim Powell <timothy.powell@eviden.com>

---
+++ src/EPR.Calculator.Service.Function.UnitTests/Models/CalcResultProducerAndReportMaterialDetailTests.cs
@@ -0,0 +1,65 @@
+namespace EPR.Calculator.Service.Function.UnitTests.Models
+{
+    using AutoFixture;
+    using EPR.Calculator.Service.Function.Data.DataModels;
+    using EPR.Calculator.Service.Function.Models;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CalcResultProducerAndReportMaterialDetailTests
+    {
+        private readonly CalcResultProducerAndReportMaterialDetail calcResultProducerAndReportMaterialDetail;
+
+        public CalcResultProducerAndReportMaterialDetailTests()
+        {
+            this.calcResultProducerAndReportMaterialDetail = new CalcResultProducerAndReportMaterialDetail
+            {
+                ProducerDetail = new ProducerDetail
+                {
+                    Id = 1,
+                    ProducerName = "Allied Packaging",
+                    CalculatorRunId = 1,
+                    CalculatorRun = new CalculatorRun { Financial_Year = "2024-25", Name = "Test Run 1" },
+                },
+                ProducerReportedMaterial = new ProducerReportedMaterial
+                {
+                    Material = new Material { Id = 1, Code = "AL", Name = "Aluminium", Description = "Aluminium" },
+                    PackagingTonnage = 1000.00m,
+                    PackagingType = "HH",
+                    MaterialId = 1,
+                    ProducerDetail = null,
+                },
+            };
+        }
+
+        [TestMethod]
+        public void CanSetAndGetProducerDetail()
+        {
+            // Arrange
+            var fixture = new Fixture();
+
+            var testValue = fixture.Create<ProducerDetail>();
+
+            // Act
+            this.calcResultProducerAndReportMaterialDetail.ProducerDetail = testValue;
+
+            // Assert
+            Assert.AreSame(testValue, this.calcResultProducerAndReportMaterialDetail.ProducerDetail);
+        }
+
+        [TestMethod]
+        public void CanSetAndGetProducerReportedMaterial()
+        {
+            // Arrange
+            var fixture = new Fixture();
+
+            var testValue = fixture.Create<ProducerReportedMaterial>();
+
+            // Act
+            this.calcResultProducerAndReportMaterialDetail.ProducerReportedMaterial = testValue;
+
+            // Assert
+            Assert.AreSame(testValue, this.calcResultProducerAndReportMaterialDetail.ProducerReportedMaterial);
+        }
+    }
+}
