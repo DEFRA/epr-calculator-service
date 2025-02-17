c38ef2f9d3056a20c3ebe4c0a0bc27532a8a389d
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2025-02-17 12:39:35 +0000

    Update CalcResultBuilder.cs

--- src/EPR.Calculator.Service.Function/Builder/CalcResultBuilder.cs
+++ src/EPR.Calculator.Service.Function/Builder/CalcResultBuilder.cs
@@ -75,7 +75,7 @@ namespace EPR.Calculator.Service.Function.Builder
             result.CalcResultParameterOtherCost = await this.calcResultParameterOtherCostBuilder.Construct(resultsRequestDto);
 
             result.CalcResultOnePlusFourApportionment = this.lapcapplusFourApportionmentBuilder.Construct(resultsRequestDto, result);
-            result.CalcResultCommsCostReportDetail = await this.commsCostReportBuilder.Construct(resultsRequestDto, result.CalcResultOnePlusFourApportionment);
+            result.CalcResultCommsCostReportDetail = await this.commsCostReportBuilder.Construct(resultsRequestDto, result.CalcResultOnePlusFourApportionment, result);
             result.CalcResultLaDisposalCostData = await this.laDisposalCostBuilder.Construct(resultsRequestDto, result);
             result.CalcResultSummary = await this.summaryBuilder.Construct(resultsRequestDto, result);
 
