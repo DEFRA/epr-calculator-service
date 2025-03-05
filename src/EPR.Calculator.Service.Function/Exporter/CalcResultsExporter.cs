dcff0890bde8382487246f1c00d8749d61ea896c
rekhadevieviden <rekha.devi.external@eviden.com>
2025-03-05 10:09:20 +0000

    Revert "Refactors the comms cost (#65)"
    This reverts commit 1929de76ce4f13db5511f05c1c477bba6ff452da.

--- src/EPR.Calculator.Service.Function/Exporter/CalcResultsExporter.cs
+++ src/EPR.Calculator.Service.Function/Exporter/CalcResultsExporter.cs
@@ -8,7 +8,6 @@
     using EPR.Calculator.Service.Function.Constants;
     using EPR.Calculator.Service.Function.Enums;
     using EPR.Calculator.Service.Function.Exporter;
-    using EPR.Calculator.Service.Function.Exporter.CommsCost;
     using EPR.Calculator.Service.Function.Exporter.LaDisposalCost;
     using EPR.Calculator.Service.Function.Exporter.OtherCosts;
     using EPR.Calculator.Service.Function.Exporter.ScaledupProducers;
@@ -47,7 +46,10 @@
 
             onePlusFourApportionmentExporter.Export(results.CalcResultOnePlusFourApportionment, csvContent);
 
-            csvContent.Append(CommsCostExporter.ExportCommsCost(results.CalcResultCommsCostReportDetail));
+            if (results.CalcResultCommsCostReportDetail != null)
+            {
+                PrepareCommsCost(results.CalcResultCommsCostReportDetail, csvContent);
+            }
 
             if (results.CalcResultLaDisposalCostData != null)
             {
@@ -65,6 +67,65 @@
             }
 
             return csvContent.ToString();
+        }
+
+        private static void PrepareCommsCost(CalcResultCommsCost communicationCost, StringBuilder csvContent)
+        {
+            csvContent.AppendLine();
+            csvContent.AppendLine();
+            csvContent.AppendLine(communicationCost.Name);
+
+            var apportionment = communicationCost.CalcResultCommsCostOnePlusFourApportionment;
+
+            foreach (var onePlusFourApportionment in apportionment)
+            {
+                csvContent.Append(CsvSanitiser.SanitiseData(onePlusFourApportionment.Name));
+                csvContent.Append(CsvSanitiser.SanitiseData(onePlusFourApportionment.England));
+                csvContent.Append(CsvSanitiser.SanitiseData(onePlusFourApportionment.Wales));
+                csvContent.Append(CsvSanitiser.SanitiseData(onePlusFourApportionment.Scotland));
+                csvContent.Append(CsvSanitiser.SanitiseData(onePlusFourApportionment.NorthernIreland));
+                csvContent.AppendLine(CsvSanitiser.SanitiseData(onePlusFourApportionment.Total));
+            }
+
+            csvContent.AppendLine();
+            var commCostByMaterials = communicationCost.CalcResultCommsCostCommsCostByMaterial;
+
+            foreach (var commCostByMaterial in commCostByMaterials)
+            {
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.Name));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.England));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.Wales));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.Scotland));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.NorthernIreland));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.Total));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.ProducerReportedHouseholdPackagingWasteTonnage));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.ReportedPublicBinTonnage));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.HouseholdDrinksContainers));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.LateReportingTonnage));
+                csvContent.Append(CsvSanitiser.SanitiseData(commCostByMaterial.ProducerReportedHouseholdPlusLateReportingTonnage));
+
+                if (commCostByMaterial.Total == CommonConstants.Total || string.IsNullOrWhiteSpace(commCostByMaterial.CommsCostByMaterialPricePerTonne))
+                {
+                    csvContent.AppendLine(CsvSanitiser.SanitiseData(commCostByMaterial.CommsCostByMaterialPricePerTonne));
+                }
+                else
+                {
+                    csvContent.AppendLine(CsvSanitiser.SanitiseData(commCostByMaterial.CommsCostByMaterialPricePerTonne, DecimalPlaces.Four, null, true, false));
+                }
+            }
+
+            csvContent.AppendLine();
+            var countryList = communicationCost.CommsCostByCountry;
+            foreach (var country in countryList)
+            {
+                csvContent.Append(CsvSanitiser.SanitiseData(country.Name));
+                csvContent.Append(CsvSanitiser.SanitiseData(country.England));
+                csvContent.Append(CsvSanitiser.SanitiseData(country.Wales));
+                csvContent.Append(CsvSanitiser.SanitiseData(country.Scotland));
+                csvContent.Append(CsvSanitiser.SanitiseData(country.NorthernIreland));
+                csvContent.Append(CsvSanitiser.SanitiseData(country.Total));
+                csvContent.AppendLine();
+            }
         }
 
         private static void PrepareOtherCosts(CalcResultParameterOtherCost otherCost, StringBuilder csvContent)
