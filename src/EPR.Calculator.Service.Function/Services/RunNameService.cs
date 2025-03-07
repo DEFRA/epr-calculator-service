2dee0212c89b7bbfc86b0ce0cf09ba96ea388d5f
Sunita Gupta <sunita.gupta.external@eviden.com>
2025-03-07 09:49:56 +0000

    remove commented code

--- src/EPR.Calculator.Service.Function/Services/RunNameService.cs
+++ src/EPR.Calculator.Service.Function/Services/RunNameService.cs
@@ -15,6 +15,11 @@
 
         private ApplicationDBContext Context { get; init; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RunNameService"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration object.</param>
+        /// <param name="context">The context object.</param>
         public RunNameService(
             IConfigurationService configuration,
             IDbContextFactory<ApplicationDBContext> context)
@@ -22,24 +27,6 @@
             this.configuration = configuration;
             this.Context = context.CreateDbContext();
         }
-
-        /*public async Task<string?> GetRunNameAsync1(int runId)
-        {
-            string? runName = null;
-            var connectionString = this.configuration.DbConnectionString;
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                await connection.OpenAsync();
-                using (var command = new SqlCommand("SELECT RunName FROM Runs WHERE RunId = @RunId", connection))
-                {
-                    command.Parameters.AddWithValue("@RunId", runId);
-                    runName = (string?)await command.ExecuteScalarAsync();
-                }
-            }
-
-            return runName;
-        }*/
 
         /// <summary>
         /// Gets the run name for the specified run ID.
