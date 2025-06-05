35de6e83e456e76b99d5b8ca2dd882137af4f7a6
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-06-05 10:26:02 +0100

    Push JsonExporter folder

---
+++ src/EPR.Calculator.Service.Function/Exporter/JsonExporter/Detail/ICalcResultDetailExporter.cs
@@ -0,0 +1,10 @@
+﻿using EPR.Calculator.Service.Function.Models;
+using System.Text;
+
+namespace EPR.Calculator.Service.Function.Exporter.JsonExporter.Detail
+{
+    public interface ICalcResultDetailExporter
+    {
+        string Export(CalcResultDetail calcResultDetail);
+    }
+}
