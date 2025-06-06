39993bf77498ebd9d38987f73c176bffe8c651ea
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-06-06 11:38:21 +0100

    Remove unused using

--- src/EPR.Calculator.Service.Function/Exporter/JsonExporter/Detail/CalcResultDetailExporter.cs
+++ src/EPR.Calculator.Service.Function/Exporter/JsonExporter/Detail/CalcResultDetailExporter.cs
@@ -1,14 +1,7 @@
-﻿using EPR.Calculator.API.Utils;
-using EPR.Calculator.Service.Function.Constants;
-using EPR.Calculator.Service.Function.Exporter.JsonExporter.Model;
-using EPR.Calculator.Service.Function.Mappers;
+﻿using EPR.Calculator.Service.Function.Mappers;
 using EPR.Calculator.Service.Function.Models;
-using Microsoft.Azure.Amqp.Encoding;
-using Microsoft.Azure.Amqp.Framing;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Text;
 
 namespace EPR.Calculator.Service.Function.Exporter.JsonExporter.Detail
 {
