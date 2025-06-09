03c82c5c3c47c552c9a3ccfbe6d9840df844e6d6
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-06-09 10:25:09 +0100

    Feature/562073 json late reporting tonnage (#115)
    * Move CsvExporter Files
    
    * Push JsonExporter folder
    
    * Moved test
    
    * Implement json exporter
    
    * add converter
    
    * Add unit test
    
    ---------
    
    Co-authored-by: David Jayakumar <david.2.jayakumar.external@eviden.com>

---
+++ src/EPR.Calculator.Service.Function.UnitTests/Exporter/JsonExporter/LateReportingTonnageTests.cs
@@ -0,0 +1,42 @@
+namespace EPR.Calculator.Service.Function.UnitTests.Exporter.JsonExporter
+{
+    using System;
+    using AutoFixture;
+    using EPR.Calculator.Service.Function.Exporter.JsonExporter;
+    using EPR.Calculator.Service.Function.Mapper;
+    using EPR.Calculator.Service.Function.Models;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class LateReportingTonnageTests
+    {
+        private LateReportingTonnage _testClass;
+        private Mock<ILateReportingTonnageMapper> _lateReportingMapper;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _lateReportingMapper = new Mock<ILateReportingTonnageMapper>();
+            _testClass = new LateReportingTonnage(_lateReportingMapper.Object);
+        }
+
+        [TestMethod]
+        public void CanCallExport()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var calcResultLateReportingData = fixture.Create<CalcResultLateReportingTonnage>();
+
+            _lateReportingMapper.Setup(mock => mock.Map(It.IsAny<CalcResultLateReportingTonnage>())).Returns(fixture.Create<CalcResultLateReportingTonnageJson>());
+
+            // Act
+            var result = _testClass.Export(calcResultLateReportingData);
+
+            // Assert
+            _lateReportingMapper.Verify(mock => mock.Map(It.IsAny<CalcResultLateReportingTonnage>()));
+
+            Assert.AreNotEqual(string.Empty, result);
+        }
+    }
+}
