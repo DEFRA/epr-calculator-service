7d557bbc2ad49c4ebde435f5566ec1c9279a5c41
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2025-06-11 13:28:27 +0100

    561698 Set run status correctly to error

--- src/EPR.Calculator.Service.Function/Startup.cs
+++ src/EPR.Calculator.Service.Function/Startup.cs
@@ -134,6 +134,7 @@ namespace EPR.Calculator.Service.Function
             services.AddTransient<ICalcResultSummaryExporter, CalcResultSummaryExporter>();
             services.AddTransient<ILateReportingExporter, LateReportingExporter>();
             services.AddTransient<IRunNameService, RunNameService>();
+            services.AddTransient<IClassificationService, ClassificationService>();
             services.AddTransient<ITelemetryClientWrapper, TelemetryClientWrapper>();
             services.AddTransient<IMessageTypeService, MessageTypeService>();
             services.AddTransient<IPrepareBillingFileService, PrepareBillingFileService>();
