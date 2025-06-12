34a7d2d67c167d64877bc6954861fe041c645930
Tim Powell <156194496+tim-powell-eviden@users.noreply.github.com>
2025-06-12 10:38:57 +0100

    562078 - Create a JSON Exporter for CalcResultSummary (#121)
    * Add JSON converter for CalcResultSummary
    
    * Add more unit tests
    
    * Fix text encoding
    
    * Fix issue with currency symbol
    
    * Expand SummaryExporter to include the calculationResults section
    
    * Rename things and tidy things up
    
    * Fix currency symbol localisation
    
    * Fix SonarQube issues
    
    * Rename a unit test class to match the class it tests

---
+++ src/EPR.Calculator.Service.Function/Converter/CurrencyConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.Service.Function.Converter
+{
+    /// <summary>
+    /// Converts a decimal value to and from a currency string representation for use in JSON serialisation.
+    /// </summary>
+    internal class CurrencyConverter : JsonConverter<decimal>
+    {
+        /// <inheritdoc/>
+        public override decimal Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.String)
+            {
+                throw new JsonException("Expected a string representing a currency value.");
+            }
+            string? currencyString = reader.GetString();
+            if (currencyString is not null
+                && currencyString.StartsWith("£", StringComparison.OrdinalIgnoreCase)
+                && decimal.TryParse(currencyString[1..], out decimal result))
+            {
+                return result;
+            }
+            throw new JsonException($"Could not parse currency value: {currencyString}");
+        }
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, decimal value, JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToString("C", CultureInfo.GetCultureInfo("en-GB")));
+    }
+}
