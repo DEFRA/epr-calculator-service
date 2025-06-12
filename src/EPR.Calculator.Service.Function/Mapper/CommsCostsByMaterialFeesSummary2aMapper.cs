ac61cee28df62863bb45bcdcc6ae885556320792
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-06-12 10:21:56 +0100

    created the class for the comms cost 2a json

---
+++ src/EPR.Calculator.Service.Function/Mapper/CommsCostsByMaterialFeesSummary2aMapper.cs
@@ -0,0 +1,18 @@
+﻿using EPR.Calculator.Service.Function.Models;
+using EPR.Calculator.Service.Function.Models.JsonExporter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.Service.Function.Mapper
+{
+    public class CommsCostsByMaterialFeesSummary2aMapper : ICommsCostsByMaterialFeesSummary2aMapper
+    {
+        public CalcResultSummaryCommsCostsByMaterialFeesSummary2a Map(CalcResultSummaryProducerDisposalFees calcResultSummaryProducerDisposalFees)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
