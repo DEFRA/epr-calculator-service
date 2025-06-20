ab4cf6d036dfd833f50db1928e2a2d16423f5e56
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-06-20 16:36:22 +0100

    feeForSASetUpCostsWithBadDebtProvision_5 (#137)
    * feeForSASetUpCostsWithBadDebtProvision_5
    
    * Fix build
    
    * Fix sonar issue

--- src/EPR.Calculator.Service.Function/Startup.cs
+++ src/EPR.Calculator.Service.Function/Startup.cs
@@ -146,6 +146,7 @@ namespace EPR.Calculator.Service.Function
             services.AddTransient<ISAOperatingCostsWithBadDebtProvisionMapper, SAOperatingCostsWithBadDebtProvisionMapper>();
             services.AddTransient<IFeeForCommsCostsWithBadDebtProvision2aMapper, FeeForCommsCostsWithBadDebtProvision2aMapper>();
             services.AddTransient<ITotalProducerFeeWithBadDebtProvisibadDebProvisionFor2con_1_2a_2b_2cMapper, TotalProducerFeeWithBadDebtProvisibadDebProvisionFor2con_1_2a_2b_2cMapper>();
+            services.AddTransient<IFeeForSASetUpCostsWithBadDebtProvision_5Mapper, FeeForSASetUpCostsWithBadDebtProvision_5Mapper>();
             services.AddTransient<ICalcResultCommsCostsWithBadDebtProvision2cMapper, CalcResultCommsCostsWithBadDebtProvision2cMapper>();
 #if !DEBUG
             SetupBlobStorage(services);
