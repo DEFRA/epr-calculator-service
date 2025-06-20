11ad2fdfa5d2658c2c8f23c615c2bea61423f01c
Shilpi Goel <Shilpi.Goel@goaco.com>
2025-06-20 12:46:49 +0100

    Changes for parametersCommsCost (onePlusFourCommsCostApportionmentPercentages)

---
+++ src/EPR.Calculator.Service.Function.UnitTests/Exporter/JsonExporter/CommsCost/CommsCostJsonExporterTests.cs
@@ -0,0 +1,76 @@
+﻿using EPR.Calculator.Service.Function.Exporter.JsonExporter;
+using EPR.Calculator.Service.Function.Mapper;
+using EPR.Calculator.Service.Function.Models;
+using EPR.Calculator.Service.Function.Models.JsonExporter;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.Service.Function.UnitTests.Exporter.JsonExporter.CommsCost
+{
+    [TestClass]
+    public class CommsCostJsonExporterTests
+    {
+        private Mock<ICommsCostMapper> mockMapper;
+        private CommsCostJsonExporter exporter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockMapper = new Mock<ICommsCostMapper>();
+            exporter = new CommsCostJsonExporter(mockMapper.Object);
+        }
+
+        [TestMethod]
+        public void Export_ShouldReturnSerializedJson()
+        {
+            // Arrange
+            var input = new CalcResultCommsCost { };
+            var mappedResult = new CalcResultCommsCostJson
+            {
+                ParametersCommsCost = new ParametersCommsCost
+                {
+                    Percentages = new OnePlusFourCommsCostApportionmentPercentages
+                    {
+                        England = "52.49321900%",
+                        Wales = "13.24848700%",
+                        Scotland = "24.32714400%",
+                        NorthernIreland = "9.93115000%",
+                        Total = "100.00000000%"
+                    }
+                }
+            };
+
+            mockMapper.Setup(m => m.Map(input)).Returns(mappedResult);
+
+            var expectedJson = JsonConvert.SerializeObject(mappedResult);
+
+            // Act
+            var actualJson = exporter.Export(input);
+
+            // Assert
+            Assert.AreEqual(expectedJson, actualJson);
+            mockMapper.Verify(m => m.Map(input), Times.Once);
+        }
+
+        [TestMethod]
+        public void Export_ShouldReturnSerializedJson_WhenMapperReturnsValidObject()
+        {
+            // Arrange  
+            var communicationCost = new CalcResultCommsCost();
+            var mappedResult = new CalcResultCommsCostJson();
+            mockMapper.Setup(m => m.Map(communicationCost)).Returns(mappedResult);
+
+            // Act  
+            var result = exporter.Export(communicationCost);
+
+            // Assert  
+            Assert.IsNotNull(result);
+            Assert.AreEqual(Newtonsoft.Json.JsonConvert.SerializeObject(mappedResult), result);
+        }
+    }
+}
---
+++ src/EPR.Calculator.Service.Function/Mapper/CommsCostMapper.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using System.Linq;
+using EPR.Calculator.Service.Function.Models;
+using EPR.Calculator.Service.Function.Models.JsonExporter;
+using Microsoft.Azure.Amqp.Framing;
+
+namespace EPR.Calculator.Service.Function.Mapper
+{
+    public class CommsCostMapper : ICommsCostMapper
+    {
+        public CalcResultCommsCostJson Map(CalcResultCommsCost calcResultCommsCost)
+        {
+            // Get the second row (index 1) which contains the actual values
+            var dataRow = calcResultCommsCost.CalcResultCommsCostOnePlusFourApportionment.Skip(1).FirstOrDefault();
+
+            if (dataRow == null)
+            {
+                return new CalcResultCommsCostJson { ParametersCommsCost = new ParametersCommsCost { Percentages = new OnePlusFourCommsCostApportionmentPercentages() } };
+            }
+
+            return new CalcResultCommsCostJson
+            {
+                ParametersCommsCost = new ParametersCommsCost
+                {
+                    Percentages = OnePlusFourCommsCostApportionmentPercentagesMap(dataRow)
+                }
+            };
+        }
+
+        private static OnePlusFourCommsCostApportionmentPercentages OnePlusFourCommsCostApportionmentPercentagesMap(CalcResultCommsCostOnePlusFourApportionment dataRow)
+        {
+
+            return new OnePlusFourCommsCostApportionmentPercentages
+            {
+                England = AppendPercent(dataRow.England),
+                Wales = AppendPercent(dataRow.Wales),
+                Scotland = AppendPercent(dataRow.Scotland),
+                NorthernIreland = AppendPercent(dataRow.NorthernIreland),
+                Total = AppendPercent(dataRow.Total)
+            };
+        }
+
+        private static string AppendPercent(string input)
+        {
+            return string.IsNullOrWhiteSpace(input)
+                ? "0.00000000%"
+                : input.Trim().EndsWith("%")
+                    ? input.Trim()
+                    : input.Trim() + "%";
+        }
+    }
+}
