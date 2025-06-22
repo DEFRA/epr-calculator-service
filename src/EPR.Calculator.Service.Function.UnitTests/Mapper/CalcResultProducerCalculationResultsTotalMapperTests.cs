00c0e866325a2becc24ee5a80887a82ac5250f8a
Jagdip Singh <jagdip.singh.external@eviden.com>
2025-06-22 17:06:44 +0100

    Add mapper tests

---
+++ src/EPR.Calculator.Service.Function.UnitTests/Mapper/CalcResultProducerCalculationResultsTotalMapperTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using EPR.Calculator.Service.Function.Mapper;
+using EPR.Calculator.Service.Function.Models;
+
+namespace EPR.Calculator.Service.Function.UnitTests.Mapper
+{
+    [TestClass]
+    public class CalcResultProducerCalculationResultsTotalMapperTests
+    {
+        private CalcResultProducerCalculationResultsTotalMapper _mapper;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _mapper = new CalcResultProducerCalculationResultsTotalMapper();
+        }
+
+        [TestMethod]
+        public void Map_ReturnsNull()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var summary = fixture.Create<CalcResultSummary>();
+
+            // Act
+            var result = _mapper.Map(summary);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
