8d055fb2972b2cbba09f03ccb711a69e9d5c5f0a
Shilpi Goel <Shilpi.Goel@goaco.com>
2025-06-22 20:29:00 +0100

    SonarQube

--- src/EPR.Calculator.Service.Function.UnitTests/Mapper/CalculationOfSuggestedBillingInstructionsAndInvoiceAmountsMapperTests.cs
+++ src/EPR.Calculator.Service.Function.UnitTests/Mapper/CalculationOfSuggestedBillingInstructionsAndInvoiceAmountsMapperTests.cs
@@ -29,5 +29,43 @@ namespace EPR.Calculator.Service.Function.UnitTests.Mapper
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void Map_ShouldMapAllPropertiesCorrectly()
+        {
+            // Arrange  
+            var fees = new CalcResultSummaryProducerDisposalFees
+            {
+                CurrentYearInvoiceTotalToDate = "1000",
+                TonnageChangeSinceLastInvoice = "50",
+                LiabilityDifference = "200",
+                MaterialThresholdBreached = "Yes",
+                TonnageThresholdBreached = "No",
+                PercentageLiabilityDifference = "10%",
+                MaterialPercentageThresholdBreached = "5%",
+                TonnagePercentageThresholdBreached = "2%",
+                SuggestedBillingInstruction = "Instruction1",
+                SuggestedInvoiceAmount = "500",
+                ProducerId = "Producer123",
+                ProducerName = "Producer Name",
+                SubsidiaryId = "Subsidiary456",
+            };
+
+            // Act  
+            var result = _testClass.Map(fees);
+
+            // Assert  
+            Assert.AreEqual("1000", result.CurrentYearInvoicedTotalToDate);
+            Assert.AreEqual("50", result.TonnageChangeSinceLastInvoice);
+            Assert.AreEqual("200", result.LiabilityDifferenceCalcVsPrev);
+            Assert.AreEqual("Yes", result.MaterialThresholdBreached);
+            Assert.AreEqual("No", result.TonnageThresholdBreached);
+            Assert.AreEqual("10%", result.PercentageLiabilityDifferenceCalcVsPrev);
+            Assert.AreEqual("5%", result.MaterialPercentageThresholdBreached);
+            Assert.AreEqual("2%", result.TonnagePercentageThresholdBreached);
+            Assert.AreEqual("Instruction1", result.SuggestedBillingInstruction);
+            Assert.AreEqual("500", result.SuggestedInvoiceAmount);
+        }
+
     }
 }
