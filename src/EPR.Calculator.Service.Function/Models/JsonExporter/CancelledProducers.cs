53bf907e9f60dedeb1f812cbb9136b8637eb8c3d
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2025-06-23 16:00:07 +0100

    Update CancelledProducers.cs

--- src/EPR.Calculator.Service.Function/Models/JsonExporter/CancelledProducers.cs
+++ src/EPR.Calculator.Service.Function/Models/JsonExporter/CancelledProducers.cs
@@ -6,10 +6,10 @@ namespace EPR.Calculator.Service.Function.Models.JsonExporter
     public record CancelledProducers
     {
         [JsonProperty(PropertyName = "name")]
-        public string Name { get; init; }
+        public string? Name { get; init; }
 
         [JsonProperty(PropertyName = "cancelledProducerTonnageInvoice")]
-        public IEnumerable<CancelledProducerTonnageInvoice> CancelledProducerTonnageInvoice { get; init; }
+        public IEnumerable<CancelledProducerTonnageInvoice>? CancelledProducerTonnageInvoice { get; init; }
     }
 
     public record CancelledProducerTonnageInvoice
@@ -18,22 +18,22 @@ namespace EPR.Calculator.Service.Function.Models.JsonExporter
         public int? ProducerId { get; init; }
 
         [JsonProperty(PropertyName = "subsidiaryId")]
-        public string SubsidiaryId { get; init; }
+        public string? SubsidiaryId { get; init; }
 
         [JsonProperty(PropertyName = "producerName")]
-        public string ProducerName { get; init; }
+        public string? ProducerName { get; init; }
 
         [JsonProperty(PropertyName = "tradingName")]
-        public string TradingName { get; init; }
+        public string? TradingName { get; init; }
 
         [JsonProperty(PropertyName = "lastProducerTonnages")]
-        public IEnumerable<LastProducerTonnages> LastProducerTonnages { get; init; }
+        public required IEnumerable<LastProducerTonnages> LastProducerTonnages { get; init; }
     }
 
     public record LastProducerTonnages
     {
         [JsonProperty(PropertyName = "materialName")]
-        public string MaterialName { get; init; }
+        public string? MaterialName { get; init; }
 
         [JsonProperty(PropertyName = "lastTonnage")]
         public decimal LastTonnage { get; init; }
