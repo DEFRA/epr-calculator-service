e8f1a0dd0b9b8c458b54b3690ea3c47dc0747691
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-06-23 10:40:15 +0100

    add unit test for exporter

--- src/EPR.Calculator.Service.Function.UnitTests/Exporter/JsonExporter/CommsCostByMaterial2A/CommsCostByMaterial2ATests.cs
+++ src/EPR.Calculator.Service.Function.UnitTests/Exporter/JsonExporter/CommsCostByMaterial2A/CommsCostByMaterial2ATests.cs
@@ -2,32 +2,35 @@ namespace EPR.Calculator.Service.Function.UnitTests.Exporter.JsonExporter.CommsC
 {
     using System;
     using System.Collections.Generic;
+    using System.Text.Json;
+    using System.Text.Json.Nodes;
     using AutoFixture;
     using EPR.Calculator.Service.Function.Exporter.JsonExporter.CommsCostByMaterial2A;
     using EPR.Calculator.Service.Function.Mapper;
     using EPR.Calculator.Service.Function.Models;
     using EPR.Calculator.Service.Function.Models.JsonExporter;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using static EPR.Calculator.Service.Common.UnitTests.Utils.JsonNodeComparer;
     using Moq;
 
     [TestClass]
     public class CommsCostByMaterial2ATests
     {
         private CommsCostByMaterial2A _testClass;
-        private Mock<ICalcResult2aCommsDataByMaterialMapper> _mapper;
+        private ICalcResult2aCommsDataByMaterialMapper _mapper;
 
         [TestInitialize]
         public void SetUp()
         {
-            _mapper = new Mock<ICalcResult2aCommsDataByMaterialMapper>();
-            _testClass = new CommsCostByMaterial2A(_mapper.Object);
+            _mapper =  new CalcResult2aCommsDataByMaterialMapper();
+            _testClass = new CommsCostByMaterial2A(_mapper);
         }
 
         [TestMethod]
         public void CanConstruct()
         {
             // Act
-            var instance = new CommsCostByMaterial2A(_mapper.Object);
+            var instance = new CommsCostByMaterial2A(_mapper);
 
             // Assert
             Assert.IsNotNull(instance);
@@ -41,16 +44,27 @@ namespace EPR.Calculator.Service.Function.UnitTests.Exporter.JsonExporter.CommsC
             var fixture = new Fixture();
             var commsCostByMaterial = fixture.Create<List<CalcResultCommsCostCommsCostByMaterial>>();
 
-            _mapper.Setup(mock => mock.Map(It.IsAny<List<CalcResultCommsCostCommsCostByMaterial>>())).Returns(fixture.Create<CalcResult2aCommsDataByMaterial>());
-
             // Act
             var result = _testClass.Export(commsCostByMaterial);
 
-            // Assert
-            _mapper.Verify(mock => mock.Map(It.IsAny<List<CalcResultCommsCostCommsCostByMaterial>>()));
+            var roundTrippedData = JsonSerializer.Deserialize<JsonObject>(result)!
+                ["calcResult2aCommsDataDetails"];
+
+            var expected = commsCostByMaterial.First();
+            var actual = roundTrippedData![0]!;
 
             Assert.IsNotNull(result);
-        }
-       
+            Assert.AreEqual(expected.ProducerReportedHouseholdPackagingWasteTonnageValue, actual["producerHouseholdPackagingWasteTonnage"].GetValue<decimal>());
+            Assert.AreEqual(expected.ReportedPublicBinTonnageValue, actual["publicBinTonnage"].GetValue<decimal>());
+            Assert.AreEqual(expected.ProducerReportedTotalTonnage, actual["totalTonnage"].GetValue<decimal>());
+            Assert.AreEqual(expected.HouseholdDrinksContainersValue, actual["householdDrinksContainersTonnage"].GetValue<decimal>());
+            Assert.AreEqual(expected.CommsCostByMaterialPricePerTonneValue, actual["commsCostByMaterialPricePerTonne"].GetValue<decimal>());
+            Assert.AreEqual(expected.EnglandValue, actual["englandCommsCost"].GetValue<decimal>());
+            Assert.AreEqual(expected.WalesValue, actual["walesCommsCost"].GetValue<decimal>());
+            Assert.AreEqual(expected.ScotlandValue, actual["scotlandCommsCost"].GetValue<decimal>());
+            Assert.AreEqual(expected.NorthernIrelandValue, actual["northernIrelandCommsCost"].GetValue<decimal>());
+            Assert.AreEqual(expected.TotalValue, actual["totalCommsCost"].GetValue<decimal>());
+            Assert.AreEqual(expected.LateReportingTonnageValue, actual["lateReportingTonnage"].GetValue<decimal>());
+        }       
     }
 }
--- src/EPR.Calculator.Service.Function/Models/JsonExporter/CalcResult2aCommsDataByMaterial.cs
+++ src/EPR.Calculator.Service.Function/Models/JsonExporter/CalcResult2aCommsDataByMaterial.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using EPR.Calculator.Service.Function.Converter;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,40 +17,63 @@ namespace EPR.Calculator.Service.Function.Models.JsonExporter
     public class CalcResult2aCommsDataDetails
     {
         [JsonProperty(PropertyName = "materialName")]
+
         public required string MaterialName { get; init; }
 
 
         [JsonProperty(PropertyName = "englandCommsCost")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal EnglandCommsCost { get; init; }
 
         [JsonProperty(PropertyName = "walesCommsCost")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal WalesCommsCost { get; init; }
 
         [JsonProperty(PropertyName = "scotlandCommsCost")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal ScotlandCommsCost { get; init; }
 
         [JsonProperty(PropertyName = "northernIrelandCommsCost")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal NorthernIrelandCommsCost { get; init; }
 
         [JsonProperty(PropertyName = "totalCommsCost")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal TotalCommsCost { get; init; }
 
         [JsonProperty(PropertyName = "producerHouseholdPackagingWasteTonnage")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal ProducerHouseholdPackagingWasteTonnage { get; init; }
 
         [JsonProperty(PropertyName = "publicBinTonnage")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal PublicBinTonnage { get; init; }
 
         [JsonProperty(PropertyName = "householdDrinksContainersTonnage")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal HouseholdDrinksContainersTonnage { get; init; }
 
         [JsonProperty(PropertyName = "lateReportingTonnage")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal LateReportingTonnage { get; init; }
 
         [JsonProperty(PropertyName = "totalTonnage")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal TotalTonnage { get; init; }
 
         [JsonProperty(PropertyName = "commsCostByMaterialPricePerTonne")]
+
+        [JsonConverter(typeof(DecimalPrecisionConverter), 3)]
         public required decimal CommsCostByMaterialPricePerTonne { get; init; }
 
     }
