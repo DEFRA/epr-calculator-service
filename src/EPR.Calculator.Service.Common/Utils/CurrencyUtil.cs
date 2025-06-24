7b630549a119e3a8e388ef42c777155c174bf5a0
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-06-24 17:10:04 +0100

    Feature/573744 commscost2a materails total (#157)
    * add comms2amateraildetailstotal
    
    * update unit tests
    
    * update unit test
    
    * update unit test
    
    * Add name

---
+++ src/EPR.Calculator.Service.Common/Utils/CurrencyUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.Service.Common.Utils
+{
+    public static class CurrencyUtil
+    {
+        public static string ConvertToCurrency(decimal detail)
+        {
+            if (detail == 0)
+            {
+                return string.Empty;
+            }
+            var culture = CultureInfo.CreateSpecificCulture("en-GB");
+            culture.NumberFormat.CurrencySymbol = "£";
+            culture.NumberFormat.CurrencyPositivePattern = 0;
+            return detail.ToString("C", culture);
+        }
+    }
+}
