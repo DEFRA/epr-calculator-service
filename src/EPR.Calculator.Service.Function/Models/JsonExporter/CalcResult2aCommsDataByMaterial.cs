b157d5ecc4ce33c7a67b8979f605f1af8b4096a5
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-06-24 15:43:52 +0100

    Add name

--- src/EPR.Calculator.Service.Function/Models/JsonExporter/CalcResult2aCommsDataByMaterial.cs
+++ src/EPR.Calculator.Service.Function/Models/JsonExporter/CalcResult2aCommsDataByMaterial.cs
@@ -7,9 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EPR.Calculator.Service.Function.Models.JsonExporter
-{
+{   
     public class CalcResult2ACommsDataByMaterial
     {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; } = "2a Comms Costs - by Material";
+
         [JsonProperty(PropertyName = "calcResult2aCommsDataDetails")]
         public required IEnumerable<CalcResult2ACommsDataDetails> CalcResult2ACommsDataDetails {  get; set; }
 
