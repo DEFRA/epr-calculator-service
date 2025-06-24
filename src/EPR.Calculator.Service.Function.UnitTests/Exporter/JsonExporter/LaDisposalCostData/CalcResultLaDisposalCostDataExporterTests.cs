a34c38d3de1c390e6bad15f280cf4cee78e74dfe
Mazar Shaik <mazar.shaik.external@eviden.com>
2025-06-24 15:54:27 +0100

    Added unit test

--- src/EPR.Calculator.Service.Function.UnitTests/Exporter/JsonExporter/LaDisposalCostData/CalcResultLaDisposalCostDataExporterTests.cs
+++ src/EPR.Calculator.Service.Function.UnitTests/Exporter/JsonExporter/LaDisposalCostData/CalcResultLaDisposalCostDataExporterTests.cs
@@ -40,7 +40,43 @@ namespace EPR.Calculator.Service.Function.UnitTests.Exporter.JsonExporter.CommsC
             var fixture = new Fixture();
             var laDisposalCostData = fixture.Create<List<CalcResultLaDisposalCostDataDetail>>();
 
-            _mapper.Setup(mock => mock.Map(It.IsAny<List<CalcResultLaDisposalCostDataDetail>>())).Returns(fixture.Create<CalcResultLaDisposalCostDataJson>());
+            var CalcResultLaDisposalCostDataJson = new CalcResultLaDisposalCostDataJson()
+            {
+                CalcResultLaDisposalCostDetails = new List<CalcResultLaDisposalCostDetails>()
+                {
+                    new CalcResultLaDisposalCostDetails()
+                    {
+                        MaterialName = "Aluminium",
+                        EnglandLaDisposalCost = "£45,000.00",
+                        WalesLaDisposalCost = "£0.00",
+                        NorthernIrelandLaDisposalCost = "£20,700.00",
+                        ScotlandLaDisposalCost = "£4,500.00",
+                        ProducerHouseholdPackagingWasteTonnage = 3899.999M,
+                        PublicBinTonnage = 666.3M,
+                        TotalLaDisposalCost = "£70,200.00",
+                        DisposalCostPricePerTonne = "£6.3396",
+                        HouseholdDrinksContainersTonnage = 507M,
+                        LateReportingTonnage = 6000M,
+                        TotalTonnage =11073.299M,
+                    }
+                },
+                CalcResultLaDisposalCostDataDetailsTotal = new CalcResultLaDisposalCostDataDetailsTotal()
+                {
+                    EnglandLaDisposalCostTotal = "£109,800.00",
+                    HouseholdDrinksContainersTonnageTotal = 507M,
+                    LateReportingTonnageTotal = 36000M,
+                    NorthernIrelandLaDisposalCostTotal = "£19,300.00",
+                    ProducerHouseholdPackagingWasteTonnageTotal = 55404.996M,
+                    PublicBinTonnage = 10947.62M,
+                    ScotlandLaDisposalCostTotal = "£49,300.00",
+                    Total = "Total",
+                    TotalLaDisposalCostTotal = "£203,150.00",
+                    TotalTonnageTotal = 102859.616M,
+                    WalesLaDisposalCostTotal = "£24,750.00"
+                }
+            };
+
+            _mapper.Setup(mock => mock.Map(It.IsAny<List<CalcResultLaDisposalCostDataDetail>>())).Returns(CalcResultLaDisposalCostDataJson);
 
             // Act
             var result = _testClass.Export(laDisposalCostData);
