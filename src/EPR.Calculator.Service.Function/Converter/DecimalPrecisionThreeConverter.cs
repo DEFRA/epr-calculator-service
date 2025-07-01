7bd5207bb42ad221304295895898f91de6003cf4
Jagdip Singh <jagdip.singh.external@eviden.com>
2025-07-01 17:29:09 +0100

    Convert underlying JSON models to use System.Text.Json Add unit tests for helper

---
+++ src/EPR.Calculator.Service.Function/Converter/DecimalPrecisionThreeConverter.cs
@@ -0,0 +1,7 @@
+﻿namespace EPR.Calculator.Service.Function.Converter
+{
+    public class DecimalPrecision3Converter : DecimalPrecisionConverter
+    {
+        public DecimalPrecision3Converter() : base(3) { }
+    }
+}
