317affaf66b19df074c8d3d1fb475bfd1a6202db
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2025-07-07 20:46:57 +0100

    579851 json exporter previous invoice tonnage

--- src/EPR.Calculator.Service.Function.UnitTests/Builder/TestDataHelper.cs
+++ src/EPR.Calculator.Service.Function.UnitTests/Builder/TestDataHelper.cs
@@ -768,7 +768,7 @@
                         WalesWithBadDebtProvision = 78.46m,
                         ScotlandWithBadDebtProvision = 156.28m,
                         NorthernIrelandWithBadDebtProvision = 61.18m,
-                        PreviousInvoicedTonnage = "110",
+                        PreviousInvoicedTonnage = "0",
                         TonnageChange = "0"
                     }
                 },
@@ -787,7 +787,7 @@
                         WalesWithBadDebtProvision = 187.96m,
                         ScotlandWithBadDebtProvision = 374.40m,
                         NorthernIrelandWithBadDebtProvision = 146.57m,
-                        PreviousInvoicedTonnage = "110",
+                        PreviousInvoicedTonnage = "0",
                         TonnageChange = "0"
                     }
                 },
@@ -807,7 +807,7 @@
                         ScotlandWithBadDebtProvision = 579.85m,
                         NorthernIrelandWithBadDebtProvision = 227,
                         HouseholdDrinksContainersTonnage = 220,
-                        PreviousInvoicedTonnage = "110",
+                        PreviousInvoicedTonnage = "0",
                         TonnageChange = "0"
                     }
                 },
@@ -826,7 +826,7 @@
                         WalesWithBadDebtProvision = 5.63m,
                         ScotlandWithBadDebtProvision = 11.21m,
                         NorthernIrelandWithBadDebtProvision = 4.39m,
-                        PreviousInvoicedTonnage = "110",
+                        PreviousInvoicedTonnage = "0",
                         TonnageChange = "0"
                     }
                 },
@@ -845,7 +845,7 @@
                         WalesWithBadDebtProvision = 1.23m,
                         ScotlandWithBadDebtProvision = 2.44m,
                         NorthernIrelandWithBadDebtProvision = 0.96m,
-                        PreviousInvoicedTonnage = "110",
+                        PreviousInvoicedTonnage = "0",
                         TonnageChange = "0"
                     }
                 },
@@ -864,7 +864,7 @@
                         WalesWithBadDebtProvision = 0.00m,
                         ScotlandWithBadDebtProvision = 0.00m,
                         NorthernIrelandWithBadDebtProvision = 0.00m,
-                        PreviousInvoicedTonnage = "110",
+                        PreviousInvoicedTonnage = "0",
                         TonnageChange = "0"
                     }
                 },
@@ -883,7 +883,7 @@
                         WalesWithBadDebtProvision = 104.60m,
                         ScotlandWithBadDebtProvision = 208.36m,
                         NorthernIrelandWithBadDebtProvision = 81.57m,
-                        PreviousInvoicedTonnage = "110",
+                        PreviousInvoicedTonnage = "0",
                         TonnageChange = "0"
                     }
                 },
@@ -902,7 +902,7 @@
                         WalesWithBadDebtProvision = 6.87m,
                         ScotlandWithBadDebtProvision = 13.68m,
                         NorthernIrelandWithBadDebtProvision = 5.36m,
-                        PreviousInvoicedTonnage = "110",
+                        PreviousInvoicedTonnage = "0",
                         TonnageChange = "0"
                     }
                 },
