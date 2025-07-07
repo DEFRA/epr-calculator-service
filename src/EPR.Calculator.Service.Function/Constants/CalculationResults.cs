570f6c6904564c8eb180e5a4edbd6d41816341af
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2025-07-07 14:34:52 +0100

    579761 json exporter org and pom file name

--- src/EPR.Calculator.Service.Function/Constants/CalculationResults.cs
+++ src/EPR.Calculator.Service.Function/Constants/CalculationResults.cs
@@ -4,7 +4,5 @@
     {
         public static readonly string DateFormat = "dd/MM/yyyy HH:mm";
         public static readonly string DateFormatISO8601 = "yyyy-MM-ddTHH:mm:ssZ";
-        public static readonly string ORG = "org_file.csv";
-        public static readonly string POM = "pom_file.csv";
     }
 }
