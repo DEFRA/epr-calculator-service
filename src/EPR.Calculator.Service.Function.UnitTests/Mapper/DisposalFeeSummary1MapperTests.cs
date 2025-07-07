66641f996399cedc06329812ca84be33efa1087f
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2025-07-07 13:22:58 +0100

    Update DisposalFeeSummary1MapperTests.cs

--- src/EPR.Calculator.Service.Function.UnitTests/Mapper/DisposalFeeSummary1MapperTests.cs
+++ src/EPR.Calculator.Service.Function.UnitTests/Mapper/DisposalFeeSummary1MapperTests.cs
@@ -14,16 +14,37 @@ namespace EPR.Calculator.Service.Function.UnitTests.Mapper
         }
 
         [TestMethod]
-        public void Map_ShouldMapValues()
+        public void Map_ShouldMapValues_ForLevelOne()
         {
             // Arrange
-            var calcResultSummaryProducerDisposalFees = TestDataHelper.GetProducerDisposalFees();
+            var calcResultSummaryProducerDisposalFees = TestDataHelper.GetProducerDisposalFees()[0];
+            calcResultSummaryProducerDisposalFees.Level = "1";
+            calcResultSummaryProducerDisposalFees.TonnageChangeCount = "-";
+            calcResultSummaryProducerDisposalFees.TonnageChangeAdvice = "-";
 
             // Act
-            var result = mapper.Map(calcResultSummaryProducerDisposalFees[0]);
+            var result = mapper.Map(calcResultSummaryProducerDisposalFees);
 
             // Assert  
             Assert.IsNotNull(result);
+            Assert.AreEqual("0", result.TonnageChangeCount);
+            Assert.AreEqual("", result.TonnageChangeAdvice);
+        }
+
+        [TestMethod]
+        public void Map_ShouldMapValues_ForLevelTwo()
+        {
+            // Arrange
+            var calcResultSummaryProducerDisposalFees = TestDataHelper.GetProducerDisposalFees()[0];
+            calcResultSummaryProducerDisposalFees.Level = "2";
+
+            // Act
+            var result = mapper.Map(calcResultSummaryProducerDisposalFees);
+
+            // Assert  
+            Assert.IsNotNull(result);
+            Assert.AreEqual("-", result.TonnageChangeCount);
+            Assert.AreEqual("-", result.TonnageChangeAdvice);
         }
     }
 }
