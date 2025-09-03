55122f81ff448046f51866fafece030c61f483e8
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-09-03 14:58:28 +0100

    cancelled producers

--- src/EPR.Calculator.Service.Function/Builder/CancelledProducers/CalcResultCancelledProducersBuilder.cs
+++ src/EPR.Calculator.Service.Function/Builder/CancelledProducers/CalcResultCancelledProducersBuilder.cs
@@ -3,16 +3,27 @@
     using System.Collections.Generic;
     using System.Globalization;
     using System.Linq;
+    using System.Security.Cryptography;
     using System.Threading.Tasks;
     using EPR.Calculator.API.Data;
+    using EPR.Calculator.API.Data.DataModels;
     using EPR.Calculator.Service.Function.Constants;
     using EPR.Calculator.Service.Function.Dtos;
     using EPR.Calculator.Service.Function.Models;
     using Microsoft.EntityFrameworkCore;
 
     public class CalcResultCancelledProducersBuilder : ICalcResultCancelledProducersBuilder
-    {        
-public async Task<CalcResultCancelledProducersResponse> Construct(CalcResultsRequestDto resultsRequestDto)
+    {
+
+
+        private readonly ApplicationDBContext context;
+
+        public CalcResultCancelledProducersBuilder(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<CalcResultCancelledProducersResponse> Construct(CalcResultsRequestDto resultsRequestDto, string financialYear)
         {
             return await Task.Run(() =>
             {
@@ -50,8 +61,79 @@ public async Task<CalcResultCancelledProducersResponse> Construct(CalcResultsReq
                     CancelledProducers = new List<CalcResultCancelledProducersDto> { topHeader }
                 };
 
+                GetCancelledProducers(financialYear, resultsRequestDto.RunId);
+
                 return response;
             });
         }
+
+        public IEnumerable<ProducerInvoicedDto> GetLatestProducerDetailsForThisFinancialYear(string financialYear)
+        {
+                var previousInvoicedNetTonnage =
+                            (from calc in context.CalculatorRuns
+                             join p in context.ProducerDesignatedRunInvoiceInstruction
+                                 on calc.Id equals p.CalculatorRunId
+                             join t in context.ProducerInvoicedMaterialNetTonnage
+                                 on new { calc.Id, p.ProducerId } equals new { Id = t.CalculatorRunId, t.ProducerId }
+                             where new int[]
+                             {
+                             RunClassificationStatusIds.INITIALRUNCOMPLETEDID,
+                             RunClassificationStatusIds.INTERMRECALCULATIONRUNCOMPID,
+                             RunClassificationStatusIds.FINALRECALCULATIONRUNCOMPID,
+                             RunClassificationStatusIds.FINALRUNCOMPLETEDID
+                             }.Contains(calc.CalculatorRunClassificationId) && calc.FinancialYearId == financialYear
+                             select new { calc, p, t })
+                            .AsEnumerable()
+                            .GroupBy(x => new { x.p.ProducerId, x.t.MaterialId })
+                            .Select(g =>
+                            {
+                                var latest = g.OrderByDescending(x => x.calc.Id).First();
+                                return new ProducerInvoicedDto
+                                {
+                                    InvoicedTonnage = latest.t,
+                                    CalculatorRun = latest.calc,
+                                    InvoiceInstruction = latest.p
+                                };
+                            })
+                            .OrderBy(x => x.InvoicedTonnage?.Id)
+                            .ThenBy(x => x.InvoicedTonnage?.ProducerId)
+                            .ToList();
+
+                return previousInvoicedNetTonnage;
+            
+        }
+
+        public IEnumerable<ProducerDetailDto> GetProducers(int runId)
+        {
+            return context.ProducerDetail.Where(t => t.CalculatorRunId == runId).
+                 Select(t => new ProducerDetailDto()
+                 {
+                     ProducerId = t.ProducerId,
+                     ProducerName = t.ProducerName,
+                 }
+                 ).ToList();
+        }
+
+
+        public void GetCancelledProducers(string financialYear, int runId)
+        {
+            var previousProducers = GetLatestProducerDetailsForThisFinancialYear(financialYear);
+            var producersForThisRun = GetProducers(runId);
+
+            var cancelledProducers = previousProducers.Where(t => !producersForThisRun.Any(k => k.ProducerId == t.InvoicedTonnage?.ProducerId));
+
+            var cancelledProducersWithData = previousProducers.Where(g => cancelledProducers.Any(f => f.InvoicedTonnage?.ProducerId == g.InvoicedTonnage?.ProducerId)).ToList();
+
+            var test = new List<CalcResultCancelledProducersDto>();
+
+            //foreach (var item in cancelledProducersWithData)
+            //{
+            //    test.Add(new CalcResultCancelledProducersDto()
+            //    {
+            //         LastTonnage = new LastTonnage() { AluminiumValue = } 
+            //    })
+            //}
+        }
+
     }
 }
