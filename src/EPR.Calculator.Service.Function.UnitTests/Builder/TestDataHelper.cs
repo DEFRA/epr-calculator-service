7ec1e6958063ef6bd09287c65d2f21c2fe476451
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2025-09-04 09:22:08 +0100

    537055 replace logic of billing instruction

--- src/EPR.Calculator.Service.Function.UnitTests/Builder/TestDataHelper.cs
+++ src/EPR.Calculator.Service.Function.UnitTests/Builder/TestDataHelper.cs
@@ -901,7 +901,7 @@
                     },
                     BillingInstructionSection = new CalcResultSummaryBillingInstruction
                     {
-                        CurrentYearInvoiceTotalToDate = 1250.89m,
+                        CurrentYearInvoiceTotalToDate = null,
                         TonnageChangeSinceLastInvoice = string.Empty,
                         LiabilityDifference = "580.73",
                         MaterialThresholdBreached = string.Empty,
