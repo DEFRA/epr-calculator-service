d5d2138f17291bdc684104b7e9686ac4cbf97446
John Jebastin Benjamin <john.2.benjamin.external@eviden.com>
2025-09-26 10:40:54 +0100

    603985 Include reject reasons (#268)
    * 603985 Include reject reasons
    
    * 603985 Include reject reasons
    
    * 603985 Include reject reasons
    
    * 603985 Include reject reasons
    
    * 603985 Include reject reasons
    
    * 603985 Include reject reasons
    
    * Update CalcResultRejectedProducersExporterTests.cs
    
    * Update CalcResultRejectedProducersBuilder.cs
    
    * Update CalcResultRejectedProducersBuilder.cs
    
    * Update CalcResultRejectedProducersBuilder.cs
    
    * Update CalcResultRejectedProducersExporter.cs
    
    * Merge from main
    
    * 603985 Include reject reasons
    
    * 603985 Include reject reasons
    
    * 603985 Include reject reasons
    
    * 603985 Include reject reasons

---
+++ src/EPR.Calculator.Service.Function/Exporter/CsvExporter/RejectedProducers/CalcResultRejectedProducersExporter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using EPR.Calculator.API.Utils;
+using EPR.Calculator.Service.Function.Constants;
+using EPR.Calculator.Service.Function.Enums;
+using EPR.Calculator.Service.Function.Models;
+
+namespace EPR.Calculator.Service.Function.Exporter.CsvExporter.RejectedProducers
+{
+    public class CalcResultRejectedProducersExporter : ICalcResultRejectedProducersExporter
+    {
+        public void Export(IEnumerable<CalcResultRejectedProducer> rejectedProducers, StringBuilder csvContent)
+        {
+            // Add empty lines
+            csvContent.AppendLine();
+            csvContent.AppendLine();
+
+            // Add headers
+            WriteHeader(csvContent);
+
+            // Add data
+            WriteData(rejectedProducers, csvContent);
+        }
+
+        private static void WriteHeader(StringBuilder csvContent)
+        {
+            csvContent.Append(CsvSanitiser.SanitiseData(CalcResultRejectedProducersHeader.RejectedReport));
+            csvContent.AppendLine();
+            csvContent.AppendLine();
+            csvContent.Append(CsvSanitiser.SanitiseData(CalcResultRejectedProducersHeader.ProducerId));
+            csvContent.Append(CsvSanitiser.SanitiseData(CalcResultRejectedProducersHeader.ProducerName));
+            csvContent.Append(CsvSanitiser.SanitiseData(CalcResultRejectedProducersHeader.TradingName));
+            csvContent.Append(CsvSanitiser.SanitiseData(CalcResultRejectedProducersHeader.SuggestedBillingInstruction));
+            csvContent.Append(CsvSanitiser.SanitiseData(CalcResultRejectedProducersHeader.SuggestedInvoiceAmount));
+            csvContent.Append(CsvSanitiser.SanitiseData(CalcResultRejectedProducersHeader.InstructionConfirmedDate));
+            csvContent.Append(CsvSanitiser.SanitiseData(CalcResultRejectedProducersHeader.InstructionConfirmedBy));
+            csvContent.Append(CsvSanitiser.SanitiseData(CalcResultRejectedProducersHeader.ReasonForReject));
+            csvContent.AppendLine();
+        }
+
+        private static void WriteData(IEnumerable<CalcResultRejectedProducer> rejectedProducers, StringBuilder csvContent)
+        {
+            foreach (var producer in rejectedProducers)
+            {
+                csvContent.Append(CsvSanitiser.SanitiseData(producer.ProducerId));
+                csvContent.Append(CsvSanitiser.SanitiseData(producer.ProducerName));
+                csvContent.Append(CsvSanitiser.SanitiseData(producer.TradingName));
+                csvContent.Append(CsvSanitiser.SanitiseData(producer.SuggestedBillingInstruction));
+                csvContent.Append(CsvSanitiser.SanitiseData(producer.SuggestedInvoiceAmount, DecimalPlaces.Two, null, true));
+                csvContent.Append(CsvSanitiser.SanitiseData(producer.InstructionConfirmedDate.ToString()));
+                csvContent.Append(CsvSanitiser.SanitiseData(producer.InstructionConfirmedBy));
+                csvContent.Append(CsvSanitiser.SanitiseData(producer.ReasonForRejection));
+                csvContent.AppendLine();
+            }
+        }
+    }
+}
