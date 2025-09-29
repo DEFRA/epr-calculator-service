e5f34c84d31be9a90455ef15c24471455656d3ff
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-09-29 17:09:57 +0100

    updated the subsidaryid to nullable

--- src/EPR.Calculator.Service.Function/Builder/Summary/TonnageVsAllProducer.cs/TotalPackagingTonnagePerRun.cs
+++ src/EPR.Calculator.Service.Function/Builder/Summary/TonnageVsAllProducer.cs/TotalPackagingTonnagePerRun.cs
@@ -3,7 +3,7 @@
     public record TotalPackagingTonnagePerRun
     {
         public int ProducerId { get; set; }
-        public required string SubsidiaryId { get; set; }
+        public string? SubsidiaryId { get; set; }
         public decimal TotalPackagingTonnage { get; set; }
     }
 }
