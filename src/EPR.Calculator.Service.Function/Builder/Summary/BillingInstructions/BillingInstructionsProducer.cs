dcf1c9ad07cf75885632432b6113f214790937c6
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-10-02 12:27:00 +0100

    Fix issue

--- src/EPR.Calculator.Service.Function/Builder/Summary/BillingInstructions/BillingInstructionsProducer.cs
+++ src/EPR.Calculator.Service.Function/Builder/Summary/BillingInstructions/BillingInstructionsProducer.cs
@@ -95,7 +95,8 @@ namespace EPR.Calculator.Service.Function.Builder.Summary.BillingInstructions
                     SuggestedInvoiceAmount = suggestedInvoiceAmount
                 };
 
-                UpdateProductPrice(context, fee.BillingInstructionSection, runId, fee.ProducerIdInt);
+                if (fee.Level == CommonConstants.LevelOne.ToString()) 
+                    UpdateProductPrice(context, fee.BillingInstructionSection, runId, fee.ProducerIdInt);
             }
         }
 
