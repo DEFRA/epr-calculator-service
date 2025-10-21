f4c4aca229e8691623eaf0049038432d88a6f5e5
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-10-21 14:29:52 +0100

    Sonar fix

--- src/EPR.Calculator.Service.Function/Builder/RejectedProducers/CalcResultRejectedProducersBuilder.cs
+++ src/EPR.Calculator.Service.Function/Builder/RejectedProducers/CalcResultRejectedProducersBuilder.cs
@@ -69,7 +69,7 @@ namespace EPR.Calculator.Service.Function.Builder.RejectedProducers
                           .OrderBy(p => p.ProducerId)
                           .ToList();
 
-            return result ?? new List<CalcResultRejectedProducer>();
+            return result;
         }
 
         private decimal GetSuggestedInvoiceAmount(ProducerResultFileSuggestedBillingInstruction billing)
