de1de4b58434ae7a1c18a384a39fe3526a05c882
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-10-27 13:13:49 +0000

    add exporter for error report

---
+++ src/EPR.Calculator.Service.Function.UnitTests/Exporter/CsvExporter/ErrorReport/CalcResultErrorReportExporterTests.cs
@@ -0,0 +1,62 @@
+﻿using EPR.Calculator.Service.Function.Constants;
+using EPR.Calculator.Service.Function.Exporter.CsvExporter.ErrorReport;
+using EPR.Calculator.Service.Function.Models;
+using Microsoft.AspNetCore.Mvc.Formatters.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.Service.Function.UnitTests.Exporter.CsvExporter.ErrorReport
+{
+    [TestClass]
+    public class CalcResultErrorReportExporterTests
+    {
+        [TestMethod]
+        public void Export_ShouldWriteExpectedHeadersToCsv()
+        {
+            // Arrange
+            var exporter = new CalcResultErrorReportExporter();
+            var stringBuilder = new StringBuilder();
+
+            var calcResultErrorReport = new List<CalcResultErrorReport>();          
+
+            // Act
+            exporter.Export(calcResultErrorReport, stringBuilder);
+            var csvOutput = stringBuilder.ToString();
+
+            // Assert
+            Assert.IsTrue(csvOutput.Contains(CommonConstants.ErrorReportHeader), "CSV should include title header.");            
+            Assert.IsTrue(csvOutput.Contains(CommonConstants.ProducerId), "CSV should include ProducerId column.");
+            Assert.IsTrue(csvOutput.Contains(CommonConstants.SubsidiaryId), "CSV should include BillingInstructionId column.");
+            Assert.IsTrue(csvOutput.Contains(CommonConstants.ProducerOrSubsidiaryName), "CSV should include TradingName column.");
+            Assert.IsTrue(csvOutput.Contains(CommonConstants.TradingName), "CSV should include Aluminium column.");
+            Assert.IsTrue(csvOutput.Contains(CommonConstants.LeaverCode), "CSV should include FibreComposite column.");
+            Assert.IsTrue(csvOutput.Contains(CommonConstants.ErrorCodeText), "CSV should include Glass column.");
+
+        }
+
+        [TestMethod]
+        public void Export_ShouldHandleEmptyErrorReport()
+        {
+            // Arrange  
+            var exporter = new CalcResultErrorReportExporter();
+            var response = new List<CalcResultErrorReport>();
+            var csvContent = new StringBuilder();
+
+            // Act  
+            exporter.Export(response, csvContent);
+
+            // Assert  
+            var result = csvContent.ToString();
+            Assert.IsTrue(result.Contains("Error"));
+            Assert.IsTrue(result.Contains("Producer ID"));
+            Assert.IsTrue(result.Contains("Subsidiary ID"));
+            Assert.IsTrue(result.Contains("Trading Name"));
+            Assert.IsTrue(result.Contains("Producer / Subsidiary Name"));
+            Assert.IsTrue(result.Contains("Leaver Code"));
+            Assert.IsTrue(result.Contains("Error Code Text"));
+        }
+    }
+}
---
+++ src/EPR.Calculator.Service.Function/Exporter/CsvExporter/ErrorReport/CalcResultErrorReportExporter.cs
@@ -0,0 +1,43 @@
+﻿using EPR.Calculator.API.Utils;
+using EPR.Calculator.Service.Function.Constants;
+using EPR.Calculator.Service.Function.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.Service.Function.Exporter.CsvExporter.ErrorReport
+{
+    public class CalcResultErrorReportExporter : ICalcResultErrorReportExporter
+    {
+        public void Export(IEnumerable<CalcResultErrorReport> errorReport, StringBuilder csvContent)
+        {
+            csvContent.AppendLine();
+            csvContent.AppendLine();
+
+            csvContent.AppendLine(CommonConstants.ErrorReportHeader);
+            WriteColumnHeaders(csvContent);
+
+            foreach (CalcResultErrorReport report in errorReport)
+            {
+                csvContent.Append(CsvSanitiser.SanitiseData(report.ProducerId));
+                csvContent.Append(CsvSanitiser.SanitiseData(report.SubsidiaryId));
+                csvContent.Append(CsvSanitiser.SanitiseData(report.ProducerName));
+                csvContent.Append(CsvSanitiser.SanitiseData(report.TradingName));
+                csvContent.Append(CsvSanitiser.SanitiseData(report.LeaverCode));
+                csvContent.Append(CsvSanitiser.SanitiseData(report.ErrorCodeText));
+            }
+        }
+
+        public static void WriteColumnHeaders(StringBuilder csvContent)
+        {
+            csvContent.Append(CsvSanitiser.SanitiseData(CommonConstants.ProducerId));
+            csvContent.Append(CsvSanitiser.SanitiseData(CommonConstants.SubsidiaryId));
+            csvContent.Append(CsvSanitiser.SanitiseData(CommonConstants.ProducerSubsidaryName));
+            csvContent.Append(CsvSanitiser.SanitiseData(CommonConstants.TradingName));
+            csvContent.Append(CsvSanitiser.SanitiseData(CommonConstants.LeaverCode));
+            csvContent.Append(CsvSanitiser.SanitiseData(CommonConstants.ErrorCodeText));
+        }
+    }
+}
