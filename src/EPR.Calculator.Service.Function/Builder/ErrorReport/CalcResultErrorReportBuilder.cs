c4ae698c28f3e75680f92287dd41ab73ceb53d47
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2025-10-31 14:39:36 +0000

    Update CalcResultErrorReportBuilder.cs

--- src/EPR.Calculator.Service.Function/Builder/ErrorReport/CalcResultErrorReportBuilder.cs
+++ src/EPR.Calculator.Service.Function/Builder/ErrorReport/CalcResultErrorReportBuilder.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Analytics.Synapse.Artifacts.Models;
 using EPR.Calculator.API.Data;
-using EPR.Calculator.API.Data.DataModels;
 using EPR.Calculator.Service.Function.Constants;
 using EPR.Calculator.Service.Function.Dtos;
 using EPR.Calculator.Service.Function.Models;
@@ -23,47 +20,36 @@ namespace EPR.Calculator.Service.Function.Builder.ErrorReport
 
         public async Task<IEnumerable<CalcResultErrorReport>> ConstructAsync(CalcResultsRequestDto resultsRequestDto)
         {
-            var errorReports = await context.ErrorReports.AsNoTracking().Where(x => x.CalculatorRunId == resultsRequestDto.RunId).ToListAsync();
-
-            var orgDetails = await  (from run in context.CalculatorRuns.AsNoTracking()
+            var query =
+                from er in context.ErrorReports
+                join run in context.CalculatorRuns on er.CalculatorRunId equals run.Id
+                join et in context.ErrorTypes on er.ErrorTypeId equals et.Id
                 join odm in context.CalculatorRunOrganisationDataMaster
                     on run.CalculatorRunOrganisationDataMasterId equals odm.Id
-                join odd in context.CalculatorRunOrganisationDataDetails on odm.Id equals odd.CalculatorRunOrganisationDataMasterId
-                                               where run.Id == resultsRequestDto.RunId
-                select odd).ToListAsync();
-
-            var result = new List<CalcResultErrorReport>();
-            foreach (var er in errorReports)
-            {
-                var calcResultErrorReport = new CalcResultErrorReport
+                join odd in context.CalculatorRunOrganisationDataDetails
+                    on new { OrgId = (int?)er.ProducerId, MasterId = odm.Id }
+                    equals new { OrgId = odd.OrganisationId, MasterId = odd.CalculatorRunOrganisationDataMasterId }
+                    into oddGroup
+                from oddLeft in oddGroup.DefaultIfEmpty()
+                where run.Id == resultsRequestDto.RunId
+                select new CalcResultErrorReport
                 {
                     Id = er.Id,
                     ProducerId = er.ProducerId,
                     SubsidiaryId = er.SubsidiaryId ?? CommonConstants.Hyphen,
+                    ProducerName = oddLeft == null ? CommonConstants.Hyphen : (oddLeft.OrganisationName ?? CommonConstants.Hyphen),
+                    TradingName = oddLeft == null ? CommonConstants.Hyphen : (oddLeft.TradingName ?? CommonConstants.Hyphen),
                     LeaverCode = er.LeaverCode ?? CommonConstants.Hyphen,
-                    ProducerName = string.Empty,
-                    TradingName = string.Empty,
-                    ErrorCodeText = string.Empty
+                    ErrorCodeText = et.Name
                 };
-                var producer = GetProducer(er, orgDetails);
-                calcResultErrorReport.ProducerName = producer?.OrganisationName ?? CommonConstants.Hyphen;
-                calcResultErrorReport.TradingName = producer?.TradingName ?? CommonConstants.Hyphen;
-                result.Add(calcResultErrorReport);
-            }
 
-            return result;
-        }
+            var results = await query
+                .AsNoTracking()
+                .GroupBy(x => x.Id)
+                .Select(g => g.First())
+                .ToListAsync();
 
-        private CalculatorRunOrganisationDataDetail GetProducer(API.Data.DataModels.ErrorReport errorReport, IEnumerable<CalculatorRunOrganisationDataDetail> orgDetails)
-        {
-            if (errorReport.SubsidiaryId == null)
-            {
-                return orgDetails.FirstOrDefault(x => x.OrganisationId == errorReport.ProducerId && x.SubsidaryId == null);
-            }
-            else
-            {
-                return orgDetails.FirstOrDefault(x => x.OrganisationId == errorReport.ProducerId && x.SubsidaryId == errorReport.SubsidiaryId);
-            }
+            return results;
         }
     }
 }
