10c1e0ec4454bc6e68f12bfc0de0dd746cd4f6f4
Mazar Shaik <mazar.shaik.external@eviden.com>
2025-11-03 11:23:22 +0000

    deleted commented code

--- src/EPR.Calculator.Service.Function/Builder/ErrorReport/CalcResultErrorReportBuilder.cs
+++ src/EPR.Calculator.Service.Function/Builder/ErrorReport/CalcResultErrorReportBuilder.cs
@@ -19,50 +19,6 @@ namespace EPR.Calculator.Service.Function.Builder.ErrorReport
             this.context = context;
         }
 
-        //public async Task<IEnumerable<CalcResultErrorReport>> ConstructAsync(CalcResultsRequestDto resultsRequestDto)
-        //{
-        //    var orgDetails = await (from run in context.CalculatorRuns.AsNoTracking()
-        //                            join odm in context.CalculatorRunOrganisationDataMaster
-        //                                on run.CalculatorRunOrganisationDataMasterId equals odm.Id
-        //                            join odd in context.CalculatorRunOrganisationDataDetails on odm.Id equals odd.CalculatorRunOrganisationDataMasterId
-        //                            where run.Id == resultsRequestDto.RunId
-        //                            select odd).ToListAsync();
-        //    bool isTradingName = false;
-        //    var query =
-        //        from er in context.ErrorReports
-        //        join run in context.CalculatorRuns on er.CalculatorRunId equals run.Id
-
-        //        join et in context.ErrorTypes on er.ErrorTypeId equals et.Id
-        //        join odm in context.CalculatorRunOrganisationDataMaster
-        //            on run.CalculatorRunOrganisationDataMasterId equals odm.Id
-        //        join odd in context.CalculatorRunOrganisationDataDetails
-        //            on new { OrgId = (int?)er.ProducerId, MasterId = odm.Id }
-        //            equals new { OrgId = odd.OrganisationId, MasterId = odd.CalculatorRunOrganisationDataMasterId }
-        //            into oddGroup
-        //        from oddLeft in oddGroup.DefaultIfEmpty()
-        //        where run.Id == resultsRequestDto.RunId
-        //        select new CalcResultErrorReport
-        //        {
-        //            Id = er.Id,
-        //            ProducerId = er.ProducerId,
-        //            SubsidiaryId = er.SubsidiaryId ?? CommonConstants.Hyphen,
-        //            ProducerName = oddLeft == null ? CommonConstants.Hyphen :
-        //                        (GetOrgOrTradingName(er.ProducerId, er.SubsidiaryId, orgDetails, isTradingName) ?? CommonConstants.Hyphen),
-        //            TradingName = oddLeft == null ? CommonConstants.Hyphen :
-        //                        (GetOrgOrTradingName(er.ProducerId, er.SubsidiaryId, orgDetails, !isTradingName) ?? CommonConstants.Hyphen),
-        //            LeaverCode = er.LeaverCode ?? CommonConstants.Hyphen,
-        //            ErrorCodeText = et.Name
-        //        };
-
-        //    var results = await query
-        //        .AsNoTracking()
-        //        .GroupBy(x => x.Id)
-        //        .Select(g => g.First())
-        //        .ToListAsync();
-
-        //    return results;
-        //}
-
         public async Task<IEnumerable<CalcResultErrorReport>> ConstructAsync(CalcResultsRequestDto resultsRequestDto)
         {
             var runId = resultsRequestDto.RunId;
