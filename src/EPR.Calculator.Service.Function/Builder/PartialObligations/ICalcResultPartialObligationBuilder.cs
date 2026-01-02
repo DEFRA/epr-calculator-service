1fd0246d8dd1a30b0fef5b7e2fd3478b4005a8a3
Ashley Wright <ashley.wright@equalexperts.com>
2026-01-02 17:48:35 +0000

    ECV-164 - Partial calculation builder to use scaled up values if available

--- src/EPR.Calculator.Service.Function/Builder/PartialObligations/ICalcResultPartialObligationBuilder.cs
+++ src/EPR.Calculator.Service.Function/Builder/PartialObligations/ICalcResultPartialObligationBuilder.cs
@@ -1,11 +1,13 @@
 ﻿namespace EPR.Calculator.Service.Function.Builder.PartialObligations
 {
+    using System.Collections.Generic;
+
     using System.Threading.Tasks;
     using EPR.Calculator.Service.Function.Dtos;
     using EPR.Calculator.Service.Function.Models;
 
     public interface ICalcResultPartialObligationBuilder
     {
-        Task<CalcResultPartialObligations> ConstructAsync(CalcResultsRequestDto resultsRequestDto);
+        Task<CalcResultPartialObligations> ConstructAsync(CalcResultsRequestDto resultsRequestDto, IEnumerable<CalcResultScaledupProducer> scaledupProducers);
     }
 }
