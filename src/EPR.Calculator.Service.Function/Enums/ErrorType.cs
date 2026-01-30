61c0a060bf674e722c710ca80c071aa68a491d8f
Nicholas Featch <nicholas.featch@equalexperts.com>
2026-01-30 09:28:38 +0000

    ECV-294 - add missing H1 and H2 messages to HandleMissingPomData

--- src/EPR.Calculator.Service.Function/Enums/ErrorType.cs
+++ src/EPR.Calculator.Service.Function/Enums/ErrorType.cs
@@ -3,7 +3,9 @@
     public static class ErrorCodes
     {
         public const string MissingRegistrationData = "Missing Registration Data";
-        public const string MissingPOMData = "Missing POM Data";
+        public const string MissingPOMData   = "Missing H1 and H2 POM Data" ;
+        public const string MissingH1POMData = "Missing H1 POM Data" ;
+        public const string MissingH2POMData = "Missing H2 POM Data" ;
         public const string Empty = "";
 
     }
