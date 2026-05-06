5aac855151302545564cf4f7bf7bdbe803555ed3
Nicholas Featch <nicholas.featch@equalexperts.com>
2026-05-06 08:48:27 +0100

    ECV-498 add ParameterServiceTests & ReportedProducerServiceTests

---
+++ src/EPR.Calculator.Service.Function.UnitTests/Services/ParameterServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using EPR.Calculator.API.Data;
+using EPR.Calculator.API.Data.DataModels;
+using EPR.Calculator.Service.Function.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace EPR.Calculator.Service.Function.UnitTests.Services
+{
+    /// <summary>
+    /// Unit tests for the <see cref="ParameterService"/> class.
+    /// </summary>
+    [TestClass]
+    public class ParameterServiceTests
+    {
+        private Mock<IDbContextFactory<ApplicationDBContext>> dbContextFactory;
+        private ApplicationDBContext dbContext;
+        private ParameterService parameterService;
+
+        public ParameterServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+                .UseInMemoryDatabase(databaseName: "ParameterTestDatabase")
+                .Options;
+
+            dbContextFactory = new Mock<IDbContextFactory<ApplicationDBContext>>();
+            dbContext = new ApplicationDBContext(options);
+
+            dbContextFactory
+                .Setup(factory => factory.CreateDbContext())
+                .Returns(dbContext);
+
+            parameterService = new ParameterService(dbContextFactory.Object);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            dbContext.Dispose();
+        }
+
+        [TestMethod]
+        public async Task ShouldReturnDefaultParameters()
+        {
+            // Arrange
+            var run = new CalculatorRun
+            {
+                Id = 1,
+                Name = "Some name",
+                DefaultParameterSettingMasterId = 100
+            };
+
+            var master = new DefaultParameterSettingMaster
+            {
+                Id = 100
+            };
+
+            var detail = new DefaultParameterSettingDetail
+            {
+                DefaultParameterSettingMasterId = 100,
+                ParameterUniqueReferenceId = "PARAM1",
+                ParameterValue = 123m
+            };
+
+            var template = new DefaultParameterTemplateMaster
+            {
+                ParameterUniqueReferenceId = "PARAM1",
+                ParameterType = "Some Type",
+                ParameterCategory = "Some Category"
+            };
+
+            dbContext.CalculatorRuns.Add(run);
+            dbContext.DefaultParameterSettings.Add(master);
+            dbContext.DefaultParameterSettingDetail.Add(detail);
+            dbContext.DefaultParameterTemplateMasterList.Add(template);
+
+            await dbContext.SaveChangesAsync();
+
+            // Act
+            var result = await parameterService.GetDefaultParameters(1);
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(123m, result["PARAM1"]);
+        }
+    }
+}
